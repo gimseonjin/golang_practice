a54d3def8ff226e01facd9f33f5efc5f4aee1d55
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-07 19:09:00 +0900

    If Done

--- ch2/type/main.go
+++ ch2/type/main.go
@@ -18,80 +18,81 @@ const (
 )
 
 func main() {
-
+	testIf()
 	/*
 
-		a := 7
-		b := 42
+			a := 7
+			b := 42
 
-		fmt.Println(a == b)
+			fmt.Println(a == b)
 
-		x = 10 < 5
+			x = 10 < 5
 
-		fmt.Println(x)
+			fmt.Println(x)
 
-		y := 12.3
-		fmt.Printf("%T\n", y)
+			y := 12.3
+			fmt.Printf("%T\n", y)
 
-		var test uint
-		fmt.Printf("%T\n", test)
-		fmt.Println(runtime.GOOS)   // darwin
-		fmt.Println(runtime.GOARCH) // amd64 -> int64 이다!!
+			var test uint
+			fmt.Printf("%T\n", test)
+			fmt.Println(runtime.GOOS)   // darwin
+			fmt.Println(runtime.GOARCH) // amd64 -> int64 이다!!
 
-		s := "hello world!"
-		fmt.Println(s)
+			s := "hello world!"
+			fmt.Println(s)
 
-		bs := []byte(s)
-		fmt.Println(bs)
-		fmt.Printf("%T\n", bs)
+			bs := []byte(s)
+			fmt.Println(bs)
+			fmt.Printf("%T\n", bs)
 
-		for i := 0; i < len(s); i++ {
-			fmt.Printf("%#U ", s[i])
-		}
+			for i := 0; i < len(s); i++ {
+				fmt.Printf("%#U ", s[i])
+			}
 
-		for i, v := range s {
-			fmt.Printf("%d, %T\n", i, v)
-		}
+			for i, v := range s {
+				fmt.Printf("%d, %T\n", i, v)
+			}
 
-		fmt.Println(string(bs))
+			fmt.Println(string(bs))
+
+
+		x := 20150190
+
+		fmt.Printf("%d\n", x)
+		fmt.Printf("%b\n", x)
+		fmt.Printf("%#x\n", x)
+
+		fmt.Println(a)
+		fmt.Println(b)
+
+		var k int8
+
+		k = a
+		fmt.Println(k)
+
+		k = b
+		fmt.Println(k)
+
+		fmt.Printf("%d\t\t\t", x<<1)
+		fmt.Printf("%b\t\t\t", x<<1)
+		fmt.Printf("%#x\t\t\t", x<<1)
+
+		a := `here is something
+		as
+		a
+		raw string
+		literal
+		"you see"
+		another thing`
+		fmt.Println(a)
+
+		fmt.Println(first)
+		fmt.Println(second)
+		fmt.Println(third)
+		fmt.Println(forth)
+
+		test()
 	*/
-
-	x := 20150190
-
-	fmt.Printf("%d\n", x)
-	fmt.Printf("%b\n", x)
-	fmt.Printf("%#x\n", x)
-
-	fmt.Println(a)
-	fmt.Println(b)
-
-	var k int8
-
-	k = a
-	fmt.Println(k)
-
-	k = b
-	fmt.Println(k)
-
-	fmt.Printf("%d\t\t\t", x<<1)
-	fmt.Printf("%b\t\t\t", x<<1)
-	fmt.Printf("%#x\t\t\t", x<<1)
-
-	a := `here is something
-	as 
-	a 
-	raw string
-	literal
-	"you see"
-	another thing`
-	fmt.Println(a)
-
-	fmt.Println(first)
-	fmt.Println(second)
-	fmt.Println(third)
-	fmt.Println(forth)
-
-	test()
 }
 
 // Go는 while 문이 없다. Only for!!!
@@ -133,5 +134,23 @@ func test() {
 		}
 
 		fmt.Println(i)
+	}
+}
+
+func testIf() {
+	if true {
+		fmt.Println("true")
+	}
+
+	if !false {
+		fmt.Println("false")
+	}
+
+	if x := 2; x == 2 {
+		fmt.Println("True")
+	} else if x == 1 {
+		fmt.Println("Else if")
+	} else {
+		fmt.Println("Else")
 	}
 }
