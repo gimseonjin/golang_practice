44312c6ee1dcd8b4a07283615cd5cff2781c31c5
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-12 15:36:24 +0900

    Done

--- ch2/type/main.go
+++ ch2/type/main.go
@@ -93,6 +93,7 @@ func main() {
 
 		test()
 	*/
+	prob9()
 }
 
 // Go는 while 문이 없다. Only for!!!
@@ -164,5 +165,104 @@ func testIf() {
 		fmt.Println("test")
 	default:
 		fmt.Println("test default ")
+	}
+}
+
+// ============ 실습 ========
+
+func prob1() {
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
+}
+
+func prob2() {
+	for i := 65; i <= 90; i++ {
+		fmt.Println(i)
+		for j := 1; j <= 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
+	}
+}
+
+func prob3() {
+	bd := 1996
+
+	for bd <= 2022 {
+		fmt.Println(bd)
+		bd++
+	}
+}
+
+func prob4() {
+	bd := 1996
+
+	for {
+		if bd > 2022 {
+			break
+		}
+		fmt.Println(bd)
+		bd++
+	}
+}
+
+func prob5() {
+	for i := 10; i <= 100; i++ {
+		fmt.Printf("%v\n", i%4)
+	}
+}
+
+func prob6() {
+	x := "James Bond"
+
+	if x == "James Bond" {
+		fmt.Println("True")
+	}
+}
+
+func prob7() {
+	x := "James Bond2"
+
+	if x == "James Bond" {
+		fmt.Println("True")
+	} else if x == "Jmes Bond2" {
+		fmt.Println("Wrong")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func prob8() {
+	switch {
+	case true:
+		fmt.Println("True")
+	case false:
+		fmt.Println("False")
+	}
+}
+
+func prob9() {
+	favSport := "suffing"
+
+	switch favSport {
+	case "swimming":
+		fmt.Println("go to river")
+	case "soccer":
+		fmt.Println("go to ground")
+	case "suffing":
+		fmt.Println("go to hawaii")
+	}
+}
+
+func prob10() {
+	favSport := "suffing"
+
+	switch favSport {
+	case "swimming":
+		fmt.Println("go to river")
+	case "soccer":
+		fmt.Println("go to ground")
+	case "suffing":
+		fmt.Println("go to hawaii")
 	}
 }
