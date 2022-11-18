07055de12207513f3ddf88fb6ec69790abbc10f9
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-18 09:47:50 +0900

    create map in go

--- fp/main.go
+++ fp/main.go
@@ -4,18 +4,24 @@ import (
 	"fmt"
 )
 
-type fn func(int) int
-
 func main() {
 
-	x := func(a int) int{
+	x := func(a int) int {
 		return a + 10
 	}
 
-	fmt.Println(test(x))
+	test := []int{1,2,3,4,5}
+
+	result := mapper(x, test)
+
+	fmt.Println(result)
 
 }
 
-func test(x fn) int {
-	return x(10)
+func mapper(f func(a int) int, iter []int) []int {
+	result := make([]int, len(iter), len(iter))
+	for i, v := range iter{
+		result[i] = f(v)
+	}
+	return result
 }
